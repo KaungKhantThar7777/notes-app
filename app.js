9d45ddf7e7dd199136cac1424441a44e943acaf7
KaungKhantThar7777 <kaungkhantthar77@gmail.com>
2020-04-18 22:59:16 +0630

    After implementing CRUD operation on file system

--- app.js
+++ app.js
@@ -4,9 +4,9 @@
 
 // fs.appendFileSync('notes.txt','I started learning NodeJs.');
 
-const validator = require('validator');
-const util = require('./utils');
-const getNotes = require('./notes');
+// const validator = require('validator');
+// const util = require('./utils');
+
 
 // getNotes();
 // console.log(util.name);
@@ -15,5 +15,71 @@ const getNotes = require('./notes');
 
 // console.log(validator.isEmail('example@example.com'));
 
-const chalk = require('chalk');
-console.log(chalk.red.bold.inverse("Fail!!!"));
+const yargs = require('yargs');
+const notes = require('./notes');
+
+
+yargs.version("1.0.1");
+yargs.command({
+    command:'add',
+    describe:'Adding a new note',
+    builder:{
+        title: {
+            describe:'Note Title',
+            demandOption:true,
+            type:'string'
+        },
+        body: {
+            describe:'Note Body',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler: function(argv) {
+        notes.addNote(argv.title,argv.body)
+    }
+})
+
+
+yargs.command({
+    command:'remove',
+    describe:'Remove a note',
+    builder:{
+        title:{
+            type:'string',
+            demandOption:true,
+            describe:'Note title'
+        }
+    },
+    handler: function(argv) {
+        notes.removeNote(argv.title);
+    }
+})
+
+
+yargs.command({
+    command:'list',
+    describe:'Listing notes',
+    handler: function() {
+        notes.listNotes();
+    }
+})
+
+
+yargs.command({
+    command:'read',
+    describe:'Reading a note',
+    builder:{
+        title:{
+            type:'string',
+            demandOption:true,
+            describe:'Note title'
+        }
+    },
+    handler: function(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse();
+// console.log(yargs.argv);
--- notes.js
+++ notes.js
@@ -1,5 +1,73 @@
-const getNotes = () => {
-    console.log('Your notes is ....');
+const fs = require('fs');
+const chalk = require('chalk')
+
+const listNotes = () => {
+    console.log(chalk.blueBright('Your notes are: \n'));
+    const notes = loadNotes();
+    notes.forEach(note => {
+        console.log(`Title: ${note.title} ${note.body}`);
+    });
+
 }
 
-module.exports = getNotes;
+const readNote = (title) => {
+    const notes = loadNotes();
+
+    const searchNote = notes.find(note => note.title === title);
+
+    if(searchNote){
+        console.log(chalk.blueBright.inverse(`Title: ${title}`));
+        console.log(searchNote.body);
+    }
+    else{
+        console.log(chalk.red('Note not Found!!!'))
+    }
+}
+const addNote = (title,body) => {
+    const notes = loadNotes();
+    
+    const duplicateNote = notes.find(note => note.title == title);
+    if(duplicateNote)
+        console.log(chalk.red.inverse('Title is already taken!'));
+    else{
+        notes.push({
+            title,body
+        })
+        saveNotes(notes);
+        console.log(chalk.red.inverse('Successfully added!'));
+    }
+
+
+
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes();
+    const removeNotes = notes.filter(note => note.title != title);
+    if(notes.length != removeNotes){
+        console.log(chalk.green.inverse('Successfully removed!'));
+    }else{
+        console.log(chalk.red.inverse(`Title: ${title} does not exist`));
+    }
+    saveNotes(removeNote);
+}
+
+const saveNotes = (notes) => fs.writeFileSync('./notes.txt',JSON.stringify(notes));
+
+const loadNotes = () => {
+    try{
+        const notesJSON = fs.readFileSync('./notes.txt');
+        return JSON.parse(notesJSON);
+    }catch(e){
+        return [];
+    }
+    
+    
+}
+
+module.exports = {
+    addNote,
+    removeNote,
+    listNotes,
+    readNote
+};
